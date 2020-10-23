82c7a236a322ac3ecd5f0f1ac14eb7ed1f789662
Ankur Singh <as.ankursingh3.1@gmail.com>
2020-10-23 16:33:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import fastai.vision.all import *
+from fastai.vision.all import *
 
-@st.cache
 def load_learner_(path):
     return load_learner(path)
 
-@st.cache
 def load_img(path):
     image = Image.open(path)
     w, h = image.size
@@ -14,15 +12,15 @@ def load_img(path):
 
 learn = load_learner_('export.pkl')
 
-st.markdown("#Animal Classifier")
+st.markdown("# Animal Classifier")
 st.markdown("Upload an image and the classifier will tell you whether its a horse, dog or bear.")
 
-file_bytes = st.file_uploader("Upload a file", type=("png", "jpg", "jpeg"))
+file_bytes = st.file_uploader("Upload a file", type=("png", "jpg", "jpeg", "jfif"))
 if file_bytes:
     img = load_img(file_bytes)
     st.image(img)
     
     submit = st.button('Predict!')
     if submit:
-        pred, pred_idx, probs = learn.predict(img)
-        st.markdown(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+        pred, pred_idx, probs = learn.predict(PILImage(img))
+        st.markdown(f'Prediction: **{pred}**; Probability: **{probs[pred_idx]:.04f}**')
